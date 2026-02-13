3bfee4e5cd36d075e49055951cbc2540d4490740
LFC Tools <lfcticketupdates@gmail.com>
2026-02-13 10:21:58 +0000

    new update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "thlfc-single-pwa-v3.6.24-r1";
+const CACHE = "thlfc-single-pwa-v4.0.0-glass";
 const ASSETS = ["./","./index.html","./manifest.json","./sw.js","./icon.svg",
   "icons/icon-192.png",
   "icons/icon-512.png",
